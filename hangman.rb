1483824556a32eff3552b879b5cf39b9bac5dc23
lujanfernaud <lujanfernaud@gmail.com>
2017-04-13 22:59:28 +0100

    Add player_wins, player_loses and refactor check_guess method

--- hangman.rb
+++ hangman.rb
@@ -48,11 +48,14 @@ class Game
   def start
     loop do
       clear_screen
+      print_guesses
+      empty_line
       # print_wrong_characters
       # empty_line
       print_hidden_word
       empty_line
       check_guess(player.input)
+      player_loses if @guesses_left.zero?
     end
   end
 
@@ -61,12 +64,23 @@ class Game
   end
 
   def check_guess(input)
-    if secret_word.include?(input)
+    if input.length == secret_word.length
+      if secret_word_is_equal_to(input)
+        player_wins
+      else
+        player_loses
+      end
+    elsif secret_word.include?(input)
       add_characters(input)
-      @guesses_left -= 1
+      player_wins if secret_word_is_equal_to(hidden_word.join)
     else
       @wrong_characters << input unless @wrong_characters.include?(input)
+      @guesses_left -= 1
     end
+  end
+
+  def secret_word_is_equal_to(input)
+    secret_word.join('') == input
   end
 
   def add_characters(input)
@@ -85,12 +99,30 @@ class Game
     puts "\n"
   end
 
+  def print_guesses
+    puts "Guesses left: #{@guesses_left}"
+  end
+
   def print_hidden_word
-    puts hidden_word.join(" ")
+    puts hidden_word.join(' ')
   end
 
   def print_wrong_characters
     puts "Wrong characters: #{@wrong_characters.join(' ')}"
+  end
+
+  def player_wins
+    clear_screen
+    puts "You WIN!\n\n"
+    puts "The correct word was: #{secret_word.join}\n\n"
+    exit
+  end
+
+  def player_loses
+    clear_screen
+    puts "You lose.\n\n"
+    puts "The correct word was: #{secret_word.join}\n\n"
+    exit
   end
 end
 
